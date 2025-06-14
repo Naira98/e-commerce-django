6fc1a2d47aee2a56ff53655b3544bb12dc5414f5
Naira98 <nairamm99@gmail.com>
2025-06-14 13:37:12 +0300

    startapp products

--- commerce/urls.py
+++ commerce/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("home/", include("home.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("home/", include("home.urls")),
+    path("products/", include("products.urls")),
+]
