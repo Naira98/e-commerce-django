eb7ef98786469b30fe7b00de444819d14602cd7e
Naira98 <nairamm99@gmail.com>
2025-06-16 11:52:26 +0300

    add product details page

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.products, name="products"),
     path("category/<str:category>", views.products_by_category, name="products_by_category"), 
+    path("details/<int:product_id>", views.product_details, name="product_details")
 ]
--- products/views.py
+++ products/views.py
@@ -18,9 +18,13 @@ def products_by_category(request, category):
     products = Product.objects.all()
     return render(
         request,
-        "products/index.html",
+        "products/products.html",
         {
             "products": products,
             "categories": [{"id": 1, "name": "Clothings"}, {"id": 2, "name": "Shoes"}],
         },
     )
+
+def product_details(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, "products/product_details.html", {"product": product})
