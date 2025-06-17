480deca356091d724151e64d49617845eba841b0
Naira98 <nairamm99@gmail.com>
2025-06-18 01:09:45 +0300

    adding pagination

--- products/views.py
+++ products/views.py
@@ -1,44 +1,50 @@
 from django.shortcuts import render
 from .models import Product, Category
+import math
 
 
 def products(request):
+    category_id = request.GET.get("category")
     try:
-        category_id = request.GET.get("category")
         category_id = int(category_id) if category_id else None
-    except ValueError:
+    except (TypeError, ValueError):
         category_id = None
 
     try:
-        page_number = int(request.GET.get("page")) 
-    except:
+        page_number = int(request.GET.get("page", 1))
+    except (TypeError, ValueError):
         page_number = 1
 
     per_page_options = [3, 6, 9]
     try:
-        per_page = int(request.GET.get("per_page")) 
+        per_page = int(request.GET.get("per_page", 6))
         if per_page not in per_page_options:
             per_page = 6
-    except:
+    except (TypeError, ValueError):
         per_page = 6
 
-
     categories = Category.objects.all()
+    filtered_products = (
+        Product.objects.filter(categories__id=category_id)
+        if category_id
+        else Product.objects.all()
+    )
 
-    if category_id:
-        products = Product.objects.filter(categories__id=category_id)
-    else:
-        products = Product.objects.all()
-
+    total_products = filtered_products.count()
+    number_of_pages = math.ceil(total_products / per_page) if per_page else 1
+    start = per_page * (page_number - 1)
+    end = start + per_page
+    products = filtered_products[start:end]
 
     context = {
-        "products": products[per_page * (page_number - 1) : page_number * per_page],
+        "products": products,
         "categories": categories,
         "selected_category": category_id,
         "per_page": per_page,
         "per_page_options": per_page_options,
+        "number_of_pages": range(1, number_of_pages + 1),
+        "page_number": page_number
     }
-
     return render(request, "products/products.html", context)
 
 
