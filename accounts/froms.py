47267fdffc31ed6b7f801b138153fb569fcea972
Naira98 <nairamm99@gmail.com>
2025-06-20 00:42:02 +0300

    add profile page and edit profile

--- accounts/froms.py
+++ accounts/froms.py
@@ -1,18 +1,25 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.hashers import make_password
 from .models import CustomUser
 
 
 class UserForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ["username", "email", "password1", "password2", "profile_picture"]
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+            "profile_picture",
+        ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        for field_name, field in self.fields.items():
+        for field in self.fields.values():
             field.widget.attrs.update(
                 {
                     "class": "w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:border-yellow-400",
@@ -24,3 +31,13 @@ class UserEditForm(ModelForm):
     class Meta:
         model = CustomUser
         fields = ["first_name", "last_name", "profile_picture"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update(
+                {
+                    "class": "w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:border-yellow-400",
+                }
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import CustomUser
-from .froms import UserForm
+from .froms import UserForm, UserEditForm
 
 
 class RegisterView(CreateView):
@@ -10,3 +11,18 @@ class RegisterView(CreateView):
     form_class = UserForm
     template_name = "registration/register.html"
     success_url = "/login"
+
+
+class UpdateProfile(UpdateView):
+    model = CustomUser
+    template_name = "registration/edit_profile.html"
+    form_class = UserEditForm
+    success_url = "/profile"
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "registration/profile.html"
