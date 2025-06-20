45d083563e0a708fd663f9b2b6c9afae41dad028
Naira98 <nairamm99@gmail.com>
2025-06-20 22:52:27 +0300

    add favorite products

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("profile/", views.ProfileView.as_view(), name="profile"),
     path("profile/edit", views.UpdateProfile.as_view(), name="edit_profile"),
+    
+    path("favorites", views.FavoritesView.as_view(), name="favorites"),
+    path("toggle-favorite", views.toggle_favorite, name="toggle-favorite"),
 ]
